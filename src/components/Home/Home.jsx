eba5cdc7c55e8bd6df95119afb70c077cb14a3e6
Kristina Tuzova <ktuzova1901@gmail.com>
2022-08-18 22:51:49 +0300

    added status for photo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import {db, auth} from '../../firebase'
-import {collection, query, where, onSnapshot, addDoc, Timestamp, orderBy,setDoc, doc, getDoc, updateDoc} from 'firebase/firestore'
+import {collection, query, where, onSnapshot, addDoc, Timestamp, orderBy,setDoc, doc} from 'firebase/firestore'
 import User from "components/User/User"
 import Profile from "components/Profile/Profile"
 import MessageForm from "components/MessageForm/MessageForm"
@@ -12,6 +12,7 @@ const Home =() => {
     const [chat, setChat] = useState("")
     const [text, setText] = useState("")
     const [msgs, setMsgs] = useState([])
+    const arrUsers =['YklieGKo6YUsMnHz8rG8C3T4iRm1', 'nqVTMcpsQcYWiGIL5bmse1KEsG02', 'yZjBmNVSvOfy6Qe8MeFj3Toj17U2']
     const user1 = auth.currentUser.uid
     useEffect(() => {
     const usersRef = collection(db, 'users')
@@ -31,16 +32,55 @@ const Home =() => {
      const id = user1 > user2? `${user1 + user2}`: `${user2 + user1}`
      const msgsRef = collection(db, 'messages', id, 'chat') 
      const q = query(msgsRef, orderBy('createdAt', 'asc'))
-     onSnapshot(q, querySnapshot => {
+      onSnapshot(q, querySnapshot => {
       let msgs = []
       querySnapshot.forEach(doc => {
         msgs.push(doc.data())
+        
       })
       setMsgs(msgs)
+   
+     
      })
-     const docSnap = await getDoc(doc(db, 'lastMsg', id));
-     if(docSnap.data()?.from !==user1) {
-      await updateDoc(doc(db,"lastMsg", id), {unread: false})
+    //  const docSnap = await getDoc(doc(db, 'lastMsg', id));
+    //  if(docSnap.data()?.from !==user1) {
+    //   await updateDoc(doc(db,"lastMsg", id), {unread: false})
+    //  }
+    }
+    async function getMessageJoke() {
+      let response = await fetch(
+        'https://api.chucknorris.io/jokes/random'
+      );
+      if (response.status === 200) {
+        let json = await response.json();
+        return json.value
+
+      }
+  
+      throw new Error(response.status);
+    }
+    async function cheakStatucUser (key)  {
+      const user2 = chat.uid;
+      const id = user1 > user2? `${user1 + user2}`: `${user2 + user1}`
+      let arr = arrUsers.find(user=> user === key)
+     if (arr!== undefined) {
+      
+      let joke = await getMessageJoke()
+      await addDoc(collection(db, 'messages', id, 'chat'), {
+        joke,
+        from: user2,
+        to: user1,
+        createdAt: Timestamp.fromDate(new Date())
+      })
+      await setDoc(doc(db, 'lastMsg', id), {
+        joke,
+        from: user1,
+        to: user2,
+        createdAt: Timestamp.fromDate(new Date()), 
+        unread: true
+      })
+      setText('')
+            
      }
     }
     const handleSubmit = async e => {
@@ -53,6 +93,9 @@ const Home =() => {
         to: user2,
         createdAt: Timestamp.fromDate(new Date())
       })
+      setTimeout(cheakStatucUser(user2), 10000)
+     
+      
       await setDoc(doc(db, 'lastMsg', id), {
         text,
         from: user1,
@@ -72,7 +115,8 @@ const Home =() => {
         </div>
         </div>
         <div className={s.messages}>{chat? <><div className={s.speaker}>
-          <img src={chat.name? avatar : chat.avatar } alt="avatar" width={'50px'} height={"50px"} className={s.photo}/>
+        <img src={chat.hasOwnProperty('avatar')? chat.avatar : avatar} alt="avatar" width={'50px'} height={"50px"} className={s.photo}/>
+        <div className={chat.isOnline? s.isOnline: s.offline}/>
         {chat.name}</div> 
         <div className={s.messagesWrapper}>
           {msgs.length? msgs.map((msg, i) => <Message key={i} msg={msg} user1={user1}/>): null}
