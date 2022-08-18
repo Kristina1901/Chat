62c18e0ff8f7a86e2cf0d21b695ccf137fcf12b4
Kristina Tuzova <ktuzova1901@gmail.com>
2022-08-18 14:49:23 +0300

    created component Navbar and styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,42 @@
 import { NavLink } from 'react-router-dom';
+import {auth, db} from '../../firebase'
+import {signOut} from 'firebase/auth'
+import {updateDoc, doc} from 'firebase/firestore'
+import { useContext } from 'react';
+import { AuthContext } from 'context/auth';
+import {useNavigate} from 'react-router-dom'
+import s from './Navbar.module.css'
+import Img from './logo.png'
 const Navbar =() => {
+  let navigate = useNavigate();
+  const {user} = useContext(AuthContext)
+  const handleSignout = async () => {
+    await updateDoc(doc(db, 'users', auth.currentUser.uid), {
+      isOnline: false
+    })
+    await signOut(auth)
+    navigate("../login", { replace: true });
+  }
     return (
-        <nav>
-          <NavLink
+      <div className={user? s.wrapper : s.container }>
+       {user?
+            <>
+            <button onClick={handleSignout} className={s.logout}>Logout</button> </> : 
+            <> <NavLink
             to="/register"
-           
-          >
-            Join
+            className={s.link}
+           >
+          <img src={Img} alt='logo' width={'50px'} height={'50px'}/>
           </NavLink>
           <NavLink
             to="/login"
-            
+            className={s.link}
           >
             Login
-          </NavLink>
-        </nav>
+          </NavLink></>}
+         
+          </div>
+        
       );
 }
 export default Navbar
