6ec4c71f0e69620cf73039d5df44b8f4bb384943
Kristina Tuzova <ktuzova1901@gmail.com>
2022-08-18 14:46:03 +0300

    create component app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,29 @@ import { lazy, Suspense, useState } from 'react';
 import { Routes, Route , Navigate} from 'react-router-dom';
 import Navbar from "./Navbar/Navbar";
 import Login from './Login/Login';
-
+import Home from './Home/Home';
+import Register from './Register/Register';
+import Profile from './Profile/Profile';
+import PrivateRoute from './PrivateRoute'
+import s from './App.module.css'
 export const App = () => {
   return (
    <>
+
+   <Navbar/>
    <Suspense fallback={<div>Download...</div>}>
     <Routes>
-    <Route path="/" element={<Login />} />
-    <Route path="*" element={<Navigate to="/" />} />          
+    
+    <Route path="/register" element={<Register />} />
+    
+    <Route path="/login" element={<Login />} />
+    <Route path="/" element={
+     <PrivateRoute redirectPath={'/register'}>
+       <Home/>
+    </PrivateRoute>
+    }
+  />
+    <Route path="*" element={<Navigate to="/register" />} />          
      </Routes>
       </Suspense>
       </>
