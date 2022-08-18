14ddce6548a2b6b73137ea458563dfb3a26add18
Kristina Tuzova <ktuzova1901@gmail.com>
2022-08-18 14:48:39 +0300

    created component Message and styles

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,4 +1,26 @@
-const Message =() => {
-    return <>Login</>
-}
-export default Message
+import Moment from "react-moment"
+import s from './Message.module.css'
+import { useRef, useEffect } from "react"
+const Message = ({msg, user1}) => {
+    const scrollRef = useRef()
+    useEffect (() => {
+      scrollRef.current?.scrollIntoView({behavior: 'smooth'})
+    }, [msg])
+    return (
+      <div className={s.main}>
+       
+    <div className={msg.from === user1? s.own : s.another}
+    ref ={scrollRef}
+    >
+    <span className={s.text}>
+    {msg.text}
+    </span> 
+      </div>  
+      <small className={s.time}>
+        <Moment format="MM/YYYY/DD, h:mm A" fromNow>{msg.createdAt.toDate()}</Moment>
+    </small>
+     </div>
+    
+    )
+   }
+   export default Message
