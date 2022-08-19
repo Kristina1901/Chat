5500bc8bdf78e4edfe47d122ea00e916b3599232
Kristina Tuzova <ktuzova1901@gmail.com>
2022-08-19 11:19:44 +0300

    Loading

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,8 +1,7 @@
+import s from './Loading.module.css'
 const Loading = () => {
  return (
-    <div>
-        Loading...
-    </div>
+    <div className={s.ring}><div></div><div></div><div></div><div></div></div>
  )
 }
 export default Loading
