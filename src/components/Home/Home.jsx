045ba1e01ba7d8aed113882aa00ffa83a1c29707
Kristina Tuzova <ktuzova1901@gmail.com>
2022-08-23 15:00:22 +0300

    fixing

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -195,7 +195,7 @@ const Home = () => {
   return (
     <div className={s.container}>
     <div className={s.home}>
-      <div className={button === true? s.sidebarPhone: s.sidebar }>
+      <div className={s.sidebar}>
         <Profile setFilterValue={setFilterValue} handleQuery={handleQuery} />
         <div className={s.wrapperUser}>
           <p className={s.title}>Chats</p>
